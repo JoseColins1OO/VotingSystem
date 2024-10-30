c29f9159253c0afb2736dcf84ebd6ae64cab3718
Jose Rios <Roraj77@hotmail.com>
2024-10-30 02:33:58 -0400

    cambios voters_modal.php

--- votesystem/admin/includes/voters_modal.php
+++ votesystem/admin/includes/voters_modal.php
@@ -10,21 +10,21 @@
             <div class="modal-body">
               <form class="form-horizontal" method="POST" action="voters_add.php" enctype="multipart/form-data">
                 <div class="form-group">
-                    <label for="firstname" class="col-sm-3 control-label">Firstname</label>
+                    <label for="firstname" class="col-sm-3 control-label">Nombre</label>
 
                     <div class="col-sm-9">
                       <input type="text" class="form-control" id="firstname" name="firstname" required>
                     </div>
                 </div>
                 <div class="form-group">
-                    <label for="lastname" class="col-sm-3 control-label">Lastname</label>
+                    <label for="lastname" class="col-sm-3 control-label">Apellido</label>
 
                     <div class="col-sm-9">
                       <input type="text" class="form-control" id="lastname" name="lastname" required>
                     </div>
                 </div>
                 <div class="form-group">
-                    <label for="password" class="col-sm-3 control-label">Password</label>
+                    <label for="password" class="col-sm-3 control-label">Contraseña</label>
 
                     <div class="col-sm-9">
                       <input type="password" class="form-control" id="password" name="password" required>
